7cd8ea2e609175087c25d9636b4e00edbb7ada14
jelereduard <jelereduard@yahoo.com>
2020-04-21 01:18:07 +0300

    Cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_115811) do
+ActiveRecord::Schema.define(version: 2020_04_20_221346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
