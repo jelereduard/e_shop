6d48e3c5038560954180b12dde7b0f0fbcd716c6
jelereduard <jelereduard@yahoo.com>
2020-04-24 17:50:54 +0300

    Moved carts to admin dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,14 @@
 class DashboardsController < ApplicationController
+  before_action :is_admin
   def dashboard
     @categories=Category.all
     @products=Product.all
+    @carts=Cart.all
+  end
+
+  def is_admin
+    if not current_user.admin?
+      redirect_to root_path
+    end
   end
 end
