c3615cf0b3763c3215f6dd622ed80b405a2fa687
jelereduard <jelereduard@yahoo.com>
2020-05-05 13:39:11 +0300

    Remove extra tabs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @products = Product.where(category_id: @category.id)
+    @products = @category.products.paginate(page: params[:page], per_page: 16)
     @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,2 @@
 module ProductsHelper
-
-  def show_category(category)
-    @cat=Category.find(category.id)
-    @products = @cat.products.paginate(page: params[:page], per_page: 16)
-  end
 end
