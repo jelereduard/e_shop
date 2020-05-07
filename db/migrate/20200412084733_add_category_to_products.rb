57bb789f49e3974e7ea39b48b3c529f13933ca83
jelereduard <jelereduard@yahoo.com>
2020-05-07 16:25:12 +0300

    Modify the migrations

--- db/migrate/20200412084733_add_category_to_products.rb
+++ db/migrate/20200412084733_add_category_to_products.rb
@@ -1,5 +1,5 @@
 class AddCategoryToProducts < ActiveRecord::Migration[5.2]
   def change
-    add_column :products, :category_id, :string
+    add_column :products, :category_id, :integer
   end
 end
--- db/migrate/20200420221346_create_carts.rb
+++ db/migrate/20200420221346_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-      t.string :user_id
+      t.integer :user_id
 
       t.timestamps
     end
